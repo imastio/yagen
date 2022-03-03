9aee4880a605427f553fdcb9f50217498a079742
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2022-03-03 16:26:45 +0400

    Added publish profiles

--- Imast.Yagen.Cli/Yagen.cs
+++ Imast.Yagen.Cli/Yagen.cs
@@ -1,4 +1,5 @@
-﻿using System.CommandLine;
+﻿using System;
+using System.CommandLine;
 using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 
@@ -13,13 +14,31 @@ namespace Imast.Yagen.Cli
         /// Generate YAML based on your recipe
         /// </summary>
         /// <param name="args">The command line arguments</param>
-        public static Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
             // create new handler
             var handler = new YagenHandler();
-            
-            // instantiate only command parser
-            return new YagenRootCommand(handler).Parse(args).InvokeAsync();
+
+            // get parsed arguments
+            var parsed = new YagenRootCommand(handler).Parse(args);
+
+            try
+            {
+                // invoke parsed commands
+                await parsed.InvokeAsync();
+            }
+            catch (YagenException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ResetColor();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Unknown Error: {e.Message}");
+                Console.ResetColor();
+            }
         }
     }
 }
